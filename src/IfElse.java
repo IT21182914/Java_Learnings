5401f4948b07707ecfc9d629c2a2370df08c2a81
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2023-12-05 16:40:21 +0530

    Change If Else

--- src/IfElse.java
+++ src/IfElse.java
@@ -3,13 +3,6 @@ public class IfElse {
 
     public static void main(String[] args) {
 
-            int x = 10;
-            int y = 20;
 
-            if (x < y) {
-                System.out.println("x is less than y");
-            } else {
-                System.out.println("x is greater than or equal to y");
-            }
     }
 }
