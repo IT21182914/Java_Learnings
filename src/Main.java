bd4bd8a8cd308bb2f7220c54ac53c2991e0be191
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2023-12-05 08:57:38 +0530

    Commit Changes in Main

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@
 public class Main {
     public static void main(String[] args) {
 
-
+         System.out.println("This is main method in Main class");
+        System.out.println("This is main method in Main class");
+        System.out.println("This is main method in Main class");
+        System.out.println("This is main method in Main class");
+        System.out.println("This is main method in Main class");
     }
 }
