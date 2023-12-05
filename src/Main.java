17e78490d8c6b74ab32fc03af78582a7eaa74040
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2023-12-05 08:58:41 +0530

    Changes in Main

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,7 @@
 public class Main {
     public static void main(String[] args) {
 
-         System.out.println("This is main method in Main class");
         System.out.println("This is main method in Main class");
-        System.out.println("This is main method in Main class");
-        System.out.println("This is main method in Main class");
-        System.out.println("This is main method in Main class");
+
     }
 }
