1b2bac690d28c9e18256573a989cc580cded33ca
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2023-12-07 01:42:51 +0530

    Solve a Problem

--- src/sample.java
+++ src/sample.java
@@ -2,10 +2,11 @@ public class sample {
 
     public static void main(String[] args) {
 
-        double doubleValue = 20.00d;
-        double doubleValue2 = 80.00d;
+        double firstValue = 20.00d;
+        double secondValue = 80.00d;
 
-        double result = (doubleValue + doubleValue2) * 100.00d;
+
+        double result = (firstValue + secondValue) * 100.00d;
         System.out.println("Result = " + result);
 
         double remainder = result % 40.00d;
@@ -14,5 +15,8 @@ public class sample {
         boolean isNoRemainder = (remainder == 0) ? true : false;
         System.out.println("isNoRemainder = " + isNoRemainder);
 
+        if (!isNoRemainder) {
+            System.out.println("Got some remainder");
+        }
     }
 }
