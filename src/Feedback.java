48cb910c9c906376d674a6c1307c4ffed051dfe5
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2023-12-06 15:09:36 +0530

    Update Feedback

--- src/Feedback.java
+++ src/Feedback.java
@@ -1,7 +1,7 @@
 public class Feedback {
     public static void main(String[] args) {
 
-        System.out.println("This is Init Feedback");
+        System.out.println("This is Updated Feedback");
 
     }
 }
