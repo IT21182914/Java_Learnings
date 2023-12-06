66f0a5bba07b22452a85f12175bb4c22a4f4b8ca
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2023-12-06 12:24:32 +0530

    Changes

--- src/IfElse.java
+++ src/IfElse.java
@@ -3,17 +3,5 @@ public class IfElse {
 
     public static void main(String[] args) {
 
-System.out.println("This is main method in IfElse class");
-
-        int x = 10;
-        int y = 20;
-
-        if (x < y) {
-            System.out.println("x is less than y");
-        } else {
-            System.out.println("x is greater than or equal to y");
-        }
-
-        System.out.println("This is the end of the program");
     }
 }
