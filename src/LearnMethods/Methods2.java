fa6b288da1187d3923076ef212a7e46e682120d1
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2023-12-20 01:59:20 +0530

    Return Type

---
+++ src/LearnMethods/Methods2.java
@@ -0,0 +1,28 @@
+package LearnMethods;
+
+public class Methods2 {
+    public static void main(String[] args) {
+
+        int highScore = calculateScore(true, 800, 5, 100);
+        System.out.println("Your final score was " + highScore);
+
+        System.out.println("Your final score was " + calculateScore(true, 10000, 8, 200));
+
+
+    }
+
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
+        int finalScore = 0;
+
+        if (gameOver) {
+
+            finalScore = score;
+
+            finalScore += (levelCompleted * bonus);
+            finalScore += 1000;
+
+        }
+        return finalScore;
+    }
+}
