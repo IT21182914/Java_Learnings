0b8003ceea3f731b23975d72b2591ffdc88ddb90
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2023-12-24 00:13:28 +0530

    Method Recap

---
+++ src/LearnMethods/IfElseRecapMethod.java
@@ -0,0 +1,22 @@
+package LearnMethods;
+
+public class IfElseRecapMethod {
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+    public static boolean isTooYoung(int age) {
+
+        boolean result = false;
+        if (age < 21) {
+           result = true;
+        }
+        return result;
+    }
+
+
+}
+
