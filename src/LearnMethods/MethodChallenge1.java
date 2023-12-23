b6e245606d5c738272afce31757a7f3f9661ce4e
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2023-12-24 01:23:43 +0530

    Modify Challenge 1

--- src/LearnMethods/MethodChallenge1.java
+++ src/LearnMethods/MethodChallenge1.java
@@ -1,48 +1,48 @@
 package LearnMethods;
 
 public class MethodChallenge1 {
-
     public static void main(String[] args) {
 
-        int highScorePosition = calculateHighScorePosition(1500);
-        displayHighScorePosition("Tim", highScorePosition);
 
-        highScorePosition = calculateHighScorePosition(900);
-        displayHighScorePosition("Bob", highScorePosition);
+        int score = calculateHighScorePosition(1500);
+        displayHighScorePosition("Tim", score);
 
-        highScorePosition = calculateHighScorePosition(400);
-        displayHighScorePosition("Percy", highScorePosition);
+        score = calculateHighScorePosition(900);
+        displayHighScorePosition("Mike", score);
 
-        highScorePosition = calculateHighScorePosition(50);
-        displayHighScorePosition("Gilbert", highScorePosition);
+        score = calculateHighScorePosition(400);
+        displayHighScorePosition("Mosh", score);
+
+        score = calculateHighScorePosition(50);
+        displayHighScorePosition("Bob", score);
 
     }
 
     public static void displayHighScorePosition(String playerName, int position) {
 
         System.out.println(playerName + " managed to get into position " + position + " on the high score list");
+
+
     }
 
-    public static int calculateHighScorePosition(int playerScore) {
+    public static int calculateHighScorePosition(int score) {
+        int result;
 
-        int result = 0;
-
-        if(playerScore >= 1000){
+        if (score >= 1000) {
 
             result = 1;
-        }
-        else if(playerScore >= 500 && playerScore < 1000){
+
+        } else if (score >= 500 && score < 1000) {
 
             result = 2;
-        }
-        else if(playerScore >= 100 && playerScore < 500){
+
+        } else if (score >= 100 && score < 500) {
 
             result = 3;
-        }
-        else{
 
-            result = 4;
-        }
+        } else result = 4;
+
+
 
         return result;
 
