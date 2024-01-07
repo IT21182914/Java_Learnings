4a6e3bc06df8b1fc13dbbab9d30555ae50ff78c5
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-01-07 19:57:48 +0530

    Abstraction

--- src/OOP/Abstraction/Shapes/Resizable.java
+++ src/OOP/Abstraction/Shapes/Resizable.java
@@ -3,5 +3,13 @@ package OOP.Abstraction.Shapes;
 // Interface
 interface Resizable {
     void resize(double factor);
+    //In Java interfaces, methods are implicitly public and abstract (except for static and default (concrete) methods),So
+    //no need to use public and abstract keywords.
+    // and variables are implicitly public, static, and final.
+
+
+    // above method is equivalent to:
+    // public abstract void resize(double factor);
+
 }
 
