c228f4f7371c0c2679437c39b8561eac9b077176
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-03-01 18:53:17 +0530

    Modify Stack

--- src/DataStructures/Stack/Main.java
+++ src/DataStructures/Stack/Main.java
@@ -4,14 +4,28 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Stack stack1 = new Stack(5);
+        stack1.push(10);
+        stack1.push(20);
+        stack1.push(30);
+        stack1.push(40);
+        stack1.push(50);
 
-        Stack stack = new Stack(5);
+        Stack stack2 = new Stack(5);
 
-        stack.push(10);
-        stack.push(20);
-        stack.push(30);
-        System.out.println("Popped: " + stack.pop());
-        System.out.println(stack.peek());
+        popAndPush(stack1, stack2);
 
+        System.out.println("Elements in stack2 after popping from stack1 and pushing into stack2:");
+        while (!stack2.isEmpty()) {
+            System.out.println(stack2.pop());
+        }
+    }
+
+    public static void popAndPush(Stack source, Stack destination) {
+        while (!source.isEmpty()) {
+            int element = source.pop();
+            destination.push(element);
+            System.out.println("Popped " + element + " from stack1 and pushed into stack2");
+        }
     }
 }
--- src/DataStructures/Stack/Stack.java
+++ src/DataStructures/Stack/Stack.java
@@ -7,34 +7,44 @@ public class Stack {
     private int top;
 
     public Stack(int s) {
-
         maxSize = s;
         stackArray = new int[maxSize];
         top = -1;
     }
 
     public void push(int j) {
-
         // check whether stack is full
-        if (top == maxSize - 1)
-            System.out.println("Stack is full");
-        else
+        if (isFull()) {
+            System.out.println("Stack is full, cannot push " + j);
+        } else {
             stackArray[++top] = j;
-        System.out.println(j + " was pushed to the stack");
+            System.out.println(j + " was pushed to the stack");
+        }
     }
 
     public int pop() {
-        if (top == -1)
-            return -99;
-        else
+        if (isEmpty()) {
+            System.out.println("Stack is empty, cannot pop");
+            return -99; // Or throw an exception
+        } else {
             return stackArray[top--];
+        }
     }
 
     public int peek() {
-        if (top == -1)
-            return -99;
-        else
+        if (isEmpty()) {
+            System.out.println("Stack is empty, no top element");
+            return -99; // Or throw an exception
+        } else {
             return stackArray[top];
+        }
     }
 
+    public boolean isFull() {
+        return (top == maxSize - 1);
+    }
+
+    public boolean isEmpty() {
+        return (top == -1);
+    }
 }
