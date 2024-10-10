f44c37eb8221197c8d953497c7ddd52de1815e77
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-10-10 18:20:49 +0530

    ODD EVEN

---
+++ src/Remainder.java
@@ -0,0 +1,15 @@
+public class Remainder {
+
+    public static void main(String[] args) {
+
+        int x = 12;
+
+        if (x % 2 == 0) {
+            System.out.println(x + " is Even number");
+        } else {
+            System.out.println(x + " is ODD number");
+        }
+
+
+    }
+}
