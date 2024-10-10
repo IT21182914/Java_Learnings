42253fa6ba485c3288efb5bd73c470c634e012e8
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-10-10 16:41:35 +0530

    Bubble Sort Algorithm

--- src/Algorithms/BubbleSort/Main.java
+++ src/Algorithms/BubbleSort/Main.java
@@ -1,4 +1,36 @@
 package Algorithms.BubbleSort;
 
 public class Main {
+
+    public static void bubbleSort(int[] arr) {
+
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                }
+
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        int[] array = {40, 20, 70, 30, 120, 150};
+        bubbleSort(array);
+
+        System.out.print("Sorted Array: ");
+        for (int i = 0; i < array.length; i++) {
+
+            System.out.print(array[i] + " ");
+        }
+
+    }
+
 }
